6179db8c4efe2fc918425a966a85acfd37a2a0c5
Adrian Kelsey <adriankelsey@Adrians-MacBook-Pro.local>
2022-09-19 15:11:26 +0100

    logic for player states

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { threadId } from 'worker_threads';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,9 +11,40 @@ export class AppController {
     return this.appService.getHello();
   }
 
+
+   players = []
+
+   playersState = []
+
+   isDuplicate = []
+
   @Post('/login')
   login(@Body() request) {
-    console.log('hello')
-    console.log(request)
+    this.players.push(request)
+    console.log(this.players)
+  }
+
+  @Get('/users')
+  getUsers() {
+    return this.players
+  }
+
+  @Post('/state')
+  postState(@Body() request) {
+    this.playersState.push(request)
+    const uuid = this.playersState.map(element => element.uuid)
+    const index = this.playersState.map(element => element.uuid).indexOf(request.uuid)
+    for(let i = 0; i < uuid.length; i++) {
+      for(let j = i; j < uuid.length; j++) {
+        if(uuid[i] === uuid[j + 1]) {
+          this.playersState.splice(i, 1)
+        }
+      }
+    }
+  }
+
+  @Get('/state')
+  getState() {
+    return this.playersState
   }
 }
